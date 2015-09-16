5aaaf7c5585a64354070bbc58112326768f7f78d
Luke Snyder <lksnyder0@gmail.com>
2015-09-16 15:54:58 -0400

    Updating Sync scripts

--- toGist.sh
+++ toGist.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+DATE=`date "+%F %T"`
+
 echo "Updating zshrc"
 cp ~/.zshrc zshrc
 echo "Updating vimrc"
 cp ~/.vimrc vimrc
+echo "Updating tmux"
+cp ~/.tmux.conf tmux.conf
+git add .
+git commit -m "Update: $DATE"
+git push origin master
--- toServer.sh
+++ toServer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+git pull
+
 echo "Updating zshrc"
 cp zshrc ~/.zshrc
 echo "Updating vimrc"
 cp vimrc ~/.vimrc
+echo "Updating tmux"
+cp tmux.conf ~/.tmux.conf
