365506316d0ee76a7b118f0bd0ee78977b654854
Luke Snyder <lksnyder0@gmail.com>
2015-09-16 16:01:52 -0400

    Moved to symlinks and added an install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone git@gist.github.com:576268fc0b81b9dd3909.git ~/.rcfile || exit 1
+
+ln -s ~/.rcfiles/vimrc ~/.vimrc
+
+ln -s ~/.rcfiles/zshrc ~/.zshrc
+
+ln -s ~/.rcfiles/.tmux.con
+
