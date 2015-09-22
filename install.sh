5580ba55b538f634eb5ca7652667cefbbd719a67
Luke Snyder <lksnyder0@gmail.com>
2015-09-21 21:27:37 -0400

    Moved clone over SSH and added force flags to symlinks

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo aptitude install vim-nox tmux zsh curl
 
 ## Install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone git@gist.github.com:576268fc0b81b9dd3909.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 ## Install OhMyZSH
@@ -12,9 +12,9 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 
 git clone https://gist.github.com/576268fc0b81b9dd3909.git ~/.rcfiles || exit 1
 
-ln -s ~/.rcfiles/vimrc ~/.vimrc
+ln -f -s ~/.rcfiles/vimrc ~/.vimrc
 
-ln -s ~/.rcfiles/zshrc ~/.zshrc
+ln -f -s ~/.rcfiles/zshrc ~/.zshrc
 
-ln -s ~/.rcfiles/tmux.conf ~/.tmux.conf
+ln -f -s ~/.rcfiles/tmux.conf ~/.tmux.conf
 
