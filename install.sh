5d969a18ff26e6979455353bc09cbb7a2065e197
Luke Snyder <lksnyder0@gmail.com>
2015-10-11 10:14:06 -0400

    adding install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo aptitude install vim-nox tmux zsh curl
+
+## Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+## Install OhMyZSH
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/lksnyder0/rcfiles.git ~/.rcfiles || exit 1
+
+ln -f -s ~/.rcfiles/vimrc ~/.vimrc
+
+ln -f -s ~/.rcfiles/zshrc ~/.zshrc
+
+ln -f -s ~/.rcfiles/tmux.conf ~/.tmux.conf
+
+vim +PluginInstall +qall
