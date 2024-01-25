13c556e2b353c4b4560908b902db5c986e4430c5
Luke Snyder <luke@ragriz.me>
2024-01-25 19:33:40 +0000

    Update install script

--- install.sh
+++ install.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
-sudo apt-get install -y vim-nox tmux zsh curl git
 
-reset
+## Setup Terraform for later
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
-## Install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo apt-get update
+sudo apt-get install -y python3-pip python3-dev tmux git zsh stow squashfuse npm terraform terraform-ls ruby ruby-dev python3-venv
 
 ## Install OhMyZSH
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-sudo usermod -s /bin/zsh luke
+## Install NPM
+curl -qL https://www.npmjs.com/install.sh | sh
+
+## Install Go
+curl -o go.tar.gz https://dl.google.com/go/go1.21.6.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go.tar.gz
+
+## Install Neovim
+mkdir ~/.local/bin
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage ~/.local/bin/
+
+## Get Solargraph (ruby lsp)
+sudo gem install solargraph
 
 git clone https://gitlab.com/lksnyder0/rcfiles.git ~/.rcfiles || exit 1
+
+cd ~/.rcfiles
+
 git submodule init
 git submodule update
 
-ln -f -s ~/.rcfiles/vimrc ~/.vimrc
+mkdir -p ~/.config
+
+ln -f -s ~/.rcfiles/nvim/config/nvim ~/.config/nvim
 
 ln -f -s ~/.rcfiles/zshrc ~/.zshrc
 
 ln -f -s ~/.rcfiles/tmux.conf ~/.tmux.conf
 
-vim +PlugInstall +qall
+cd nvim
+
+python3 -m pip install requirements.devbox.txt
+
+cd ~/.config/nvim
