3bffb0f977d024f980c61d5ba6638eacfa55535d
Luke Snyder <lksnyder0@gmail.com>
2024-07-15 14:48:29 -0400

    Update nvim plugins

--- nvim/config/nvim/lua/plugins/which-key.lua
+++ nvim/config/nvim/lua/plugins/which-key.lua
@@ -1,23 +1,16 @@
 return {
 	{
 		"folke/which-key.nvim",
-		config = function()
-			local wk = require("which-key")
-			wk.setup()
-			wk.register(
-				{
-					["<leader>"] = {
-						f = { name = "File" },
-						d = { name = "Delete/Close" },
-						q = { name = "Quit" },
-						s = { name = "Search" },
-						l = { name = "LSP" },
-						u = { name = "UI" },
-						b = { name = "Debugging" },
-						g = { name = "Git" },
-					}
-				}
-			)
-		end
-	}
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
 }
