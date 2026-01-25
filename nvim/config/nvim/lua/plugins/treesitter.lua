c736998397f863550e0f72532bff4adf0aa3bce6
Luke Snyder <709836+lksnyder0@users.noreply.github.com>
2026-01-24 23:04:27 -0500

    Update nvim plugins

--- nvim/config/nvim/lua/plugins/treesitter.lua
+++ nvim/config/nvim/lua/plugins/treesitter.lua
@@ -6,11 +6,11 @@ return {
 			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
 		end,
 		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
+			-- "nvim-treesitter/nvim-treesitter-textobjects",
 			"nvim-treesitter/nvim-treesitter-context",
 		},
 		config = function()
-			require("nvim-treesitter.configs").setup({
+			require("nvim-treesitter.config").setup({
 				-- Add languages to be installed here that you want installed for treesitter
 				ensure_installed = {
 					"go",
@@ -41,41 +41,42 @@ return {
 						node_decremental = "<c-backspace>",
 					},
 				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = "@class.outer",
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-							["]["] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[M"] = "@function.outer",
-							["[]"] = "@class.outer",
-						},
-					},
-				},
+				-- I need to understand this more.
+				-- textobjects = {
+				-- 	select = {
+				-- 		enable = true,
+				-- 		lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				-- 		keymaps = {
+				-- 			-- You can use the capture groups defined in textobjects.scm
+				-- 			["aa"] = "@parameter.outer",
+				-- 			["ia"] = "@parameter.inner",
+				-- 			["af"] = "@function.outer",
+				-- 			["if"] = "@function.inner",
+				-- 			["ac"] = "@class.outer",
+				-- 			["ic"] = "@class.inner",
+				-- 		},
+				-- 	},
+				-- 	move = {
+				-- 		enable = true,
+				-- 		set_jumps = true, -- whether to set jumps in the jumplist
+				-- 		goto_next_start = {
+				-- 			["]m"] = "@function.outer",
+				-- 			["]]"] = "@class.outer",
+				-- 		},
+				-- 		goto_next_end = {
+				-- 			["]M"] = "@function.outer",
+				-- 			["]["] = "@class.outer",
+				-- 		},
+				-- 		goto_previous_start = {
+				-- 			["[m"] = "@function.outer",
+				-- 			["[["] = "@class.outer",
+				-- 		},
+				-- 		goto_previous_end = {
+				-- 			["[M"] = "@function.outer",
+				-- 			["[]"] = "@class.outer",
+				-- 		},
+				-- 	},
+				-- },
 			})
 		end,
 	},
